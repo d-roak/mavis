550a43082e4c8d2312e7d113a5872d5d282c47a3
droak <me+git@droak.sh>
2024-04-23 05:49:00 +0900

    add macros to impl to common numbers

--- crates/crdts/src/counters/g_counter.rs
+++ crates/crdts/src/counters/g_counter.rs
@@ -17,31 +17,39 @@ impl<T> GCounter<T> {
     }
 }
 
-impl Counter<u64, GCounter<u64>> for GCounter<u64> {
-    fn increment(&mut self) {
-        self.entries[self.id] += 1;
-    }
+macro_rules! impl_GCounter {
+    ( $($type:ty),* $(,)? ) => {
+        $(
+            impl Counter<$type, GCounter<$type>> for GCounter<$type> {
+                fn increment(&mut self) {
+                    self.entries[self.id] += 1;
+                }
 
-    fn decrement(&mut self) {
-        unimplemented!("grow only counters can't decrement")
-    }
+                fn decrement(&mut self) {
+                    unimplemented!("grow only counters can't decrement")
+                }
 
-    fn value(&self) -> u64 {
-        self.entries.iter().sum()
-    }
+                fn value(&self) -> $type {
+                    self.entries.iter().sum()
+                }
 
-    fn compare(&self, counter: GCounter<u64>) -> bool {
-        for (i, entry) in self.entries.iter().enumerate() {
-            if *entry != counter.entries[i] {
-                return false;
+                fn compare(&self, counter: GCounter<$type>) -> bool {
+                    for (i, entry) in self.entries.iter().enumerate() {
+                        if *entry != counter.entries[i] {
+                            return false;
+                        }
+                    }
+                    true
+                }
+
+                fn merge(&mut self, counter: GCounter<$type>) {
+                    for (i, entry) in self.entries.iter_mut().enumerate() {
+                        *entry = std::cmp::max(*entry, counter.entries[i])
+                    }
+                }
             }
-        }
-        true
-    }
-
-    fn merge(&mut self, counter: GCounter<u64>) {
-        for (i, entry) in self.entries.iter_mut().enumerate() {
-            *entry = std::cmp::max(*entry, counter.entries[i])
-        }
-    }
+        )*
+    };
 }
+
+impl_GCounter!(u8, u16, u32, u64, u128, usize);
--- crates/crdts/src/counters/pn_counter.rs
+++ crates/crdts/src/counters/pn_counter.rs
@@ -19,32 +19,40 @@ impl<T> PNCounter<T> {
     }
 }
 
-impl Counter<u64, PNCounter<u64>> for PNCounter<u64> {
-    fn increment(&mut self) {
-        self.increments[self.id] += 1;
-    }
+macro_rules! impl_PNCounter {
+    ( $($type:ty),* $(,)? ) => {
+        $(
+            impl Counter<$type, PNCounter<$type>> for PNCounter<$type> {
+                fn increment(&mut self) {
+                    self.increments[self.id] += 1;
+                }
 
-    fn decrement(&mut self) {
-        self.decrements[self.id] += 1;
-    }
+                fn decrement(&mut self) {
+                    self.decrements[self.id] += 1;
+                }
 
-    fn value(&self) -> u64 {
-        self.increments.iter().sum::<u64>() - self.decrements.iter().sum::<u64>()
-    }
+                fn value(&self) -> $type {
+                    self.increments.iter().sum::<$type>() - self.decrements.iter().sum::<$type>()
+                }
 
-    fn compare(&self, counter: PNCounter<u64>) -> bool {
-        for (i, entry) in self.increments.iter().enumerate() {
-            if *entry != counter.increments[i] || self.decrements[i] != counter.decrements[i] {
-                return false;
+                fn compare(&self, counter: PNCounter<$type>) -> bool {
+                    for (i, entry) in self.increments.iter().enumerate() {
+                        if *entry != counter.increments[i] || self.decrements[i] != counter.decrements[i] {
+                            return false;
+                        }
+                    }
+                    true
+                }
+
+                fn merge(&mut self, counter: PNCounter<$type>) {
+                    for (i, entry) in self.increments.iter_mut().enumerate() {
+                        *entry = std::cmp::max(*entry, counter.increments[i]);
+                        self.decrements[i] = std::cmp::max(self.decrements[i], counter.decrements[i]);
+                    }
+                }
             }
-        }
-        true
-    }
-
-    fn merge(&mut self, counter: PNCounter<u64>) {
-        for (i, entry) in self.increments.iter_mut().enumerate() {
-            *entry = std::cmp::max(*entry, counter.increments[i]);
-            self.decrements[i] = std::cmp::max(self.decrements[i], counter.decrements[i]);
-        }
-    }
+        )*
+    };
 }
+
+impl_PNCounter!(u8, u16, u32, u64, u128, usize);
